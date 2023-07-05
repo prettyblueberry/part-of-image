cbbba5ee5c5606a2f464fd40f691e1110fc2e90d
prettyblueberry <prettyblueberry52702@gmail.com>
2023-07-05 10:42:46 +0200

    cut 2kb of image(png, jpeg; not svg)

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const https = require('https');
+const { Buffer } = require('node:buffer');
+
+// image path: You can change url of image
+const imageURL = 'https://placehold.co/600x400/000000/FFFFFF/jpeg';
+
+// size limit 2kb
+const sizeLimit = 2048;
 
 const app = express();
 
-app.get('/image.png', (req, res) => {
-  // const url = req.query.url;
-  const url = 'https://placehold.co/600x400/png';
-  const proxyReq = https.get(url, (proxyRes) => {
-    proxyRes.pipe(res);
+//image routing
+app.get('/image', (req, res) => {
+  //make a request
+  const proxyReq = https.get(imageURL, (proxyRes) => {
+    let data = Buffer.from([]);
+    proxyRes.on('data', (chunk) => {
+      if (data.length < sizeLimit) {
+        data = Buffer.concat([data, chunk], data.length + chunk.length);
+      } else {
+        proxyRes.destroy();
+      }
+    });
+    proxyRes.on('end', () => {
+      // only 2kb
+      if (data.length >= sizeLimit) res.send(data.subarray(0, sizeLimit));
+      else res.send(data);
+    });
   });
 
   proxyReq.on('error', (err) => {
     res.status(500).send('Error downloading image');
   });
 });
+
+//root routing: client html
 app.get('/', (req, res) => {
-  res.send("<img src='/image.png'>");
+  res.send("<img src='/image'>");
 });
+
+//host
 app.listen(3000);
